cf21a69774ed3c29bbd48497df540262ab7ea8e5
PreciousAgboghai <agboghaiprecious@gmail.com>
2023-03-21 14:55:37 -0700

    alphabelt

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints a-z
+ *
+ * a function that prints the alphabet, in lowercase, followed by a new line.
+ *
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+}
